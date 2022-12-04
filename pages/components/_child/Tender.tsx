456162180354966fc56bfa140b56dc9fb917025d
Okoli Nnaemeka <okolinnaemeka227@gmail.com>
2022-12-04 14:31:32 +0100

    padding fix

--- pages/components/_child/Tender.tsx
+++ pages/components/_child/Tender.tsx
@@ -12,11 +12,11 @@ function Tender(props: PropType) {
     const { image, name, opportunity } = props;
 
     return (
-        <div className='flex flex-row items-center px-3 my-3 rounded-md justify-between border-2'>
+        <div className='flex flex-row items-center px-3 my-3 py-9 rounded-md justify-between border-2'>
             <Image src={image} alt={name} />
             <div className='items-start basis-3/4'>
                 <b>{name}</b><br />
-                <span>Tender Opportunity:</span><span>{opportunity}</span>
+                <span>Tender Opportunity:</span><span>{` ${opportunity}`}</span>
             </div>
             <div>
                 <Button text='View Details' />
