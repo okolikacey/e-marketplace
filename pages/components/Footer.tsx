803a217a186069319ad2abca36e2345c0035abdc
Okoli Nnaemeka <okolinnaemeka227@gmail.com>
2022-12-05 16:45:01 +0100

    Finished footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -5,6 +5,17 @@ function Footer(props) {
     return (
         <footer>
             <Newsletter />
+            <div className='bg-cblack text-white text-lg flex flex-row py-10 px-10 justify-between'>
+                <div>
+                    © 2022. «NCDMB». All Right Reserved. Powered by VASCON Solutions
+                </div>
+                <div className='flex flex-row gap-14'>
+                    <a>Advertise</a>
+                    <a>Privacy</a>
+                    <a>Work with us</a>
+                    <a>FAQ's</a>
+                </div>
+            </div>
         </footer>
     );
 }
