06fd47c1732aa70bb2b970a16fd23ef8bf328693
Okoli Nnaemeka <okolinnaemeka227@gmail.com>
2022-12-06 11:16:32 +0100

    Added style to active navigation links

--- components/_child/NavOptions.tsx
+++ components/_child/NavOptions.tsx
@@ -1,18 +1,20 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { RiArrowDownSLine } from 'react-icons/ri'
 
 function NavOptions(props) {
+    const router = useRouter()
     return (
         <div className='px-20 py-5 flex flex-row flex-wrap text-xs justify-between mt-0.5 bg-white'>
             <div className='flex flex-row gap-4 items-center'>
-                <Link href={'/'}>HOME</Link>
+                <Link href={'/'} className={router.pathname == "/" ? "text-cgreen" : ""}>HOME</Link>
                 <div className='flex flex-row items-center gap-2'>
                     CATEGORIES
                     <RiArrowDownSLine />
                 </div>
                 <div>ADVERTS</div>
-                <Link href={'/productsandservices'}>PRODUCTS AND SERVICES</Link>
+                <Link href={'/productsandservices'} className={router.pathname == "/productsandservices" ? "text-cgreen" : ""}>PRODUCTS AND SERVICES</Link>
                 <div>COMPANIES</div>
             </div>
             <div className='flex flex-row gap-4'>
